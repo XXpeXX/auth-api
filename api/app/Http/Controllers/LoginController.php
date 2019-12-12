f0809f952d0b3c38386aa77a0d51099c9741b06a
Pedro <pedro_soto_aaps1ta1819@cev.com>
2019-12-12 16:53:33 +0100

    decoded data

--- api/app/Http/Controllers/LoginController.php
+++ api/app/Http/Controllers/LoginController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \Firebase\JWT\JWT;
 
 class LoginController extends Controller
 {
-    public function test(Request $request){
-        return $request;
+    public function login(Request $request){
+
+        $decoded = JWT::decode($request->getContent(), env('JWT_KEY'), array('HS256'));
+
+        $decoded_array = (array) $decoded;
+
+        return $decoded_array;
     }
 }
